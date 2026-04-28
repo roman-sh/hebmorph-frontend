bd2d432be877bc8b9355b69e5f2fb5d2b09d2619
Roman Shmigelsky <shmigelsky@gmail.com>
2026-04-29 01:49:11 +0300

    content updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,24 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://teivah.solutions',
   integrations: [
       starlight({
-          title: 'My Docs',
-          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+          title: 'Hebrew Lemmatization API',
+          social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/roman-sh/hebMorph-service' }],
           sidebar: [
               {
-                  label: 'Guides',
+                  label: 'API Reference',
                   items: [
-                      // Each item here is one entry in the navigation menu.
-                      { label: 'Example Guide', slug: 'guides/example' },
+                      { label: 'Overview', slug: '' },
                   ],
               },
               {
-                  label: 'Reference',
-                  autogenerate: { directory: 'reference' },
+                  label: 'Support & Legal',
+                  items: [
+                      { label: 'Contact Us', slug: 'support' },
+                      { label: 'Terms & Privacy', slug: 'terms' },
+                  ],
               },
           ],
       }),
